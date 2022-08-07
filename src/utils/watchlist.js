12c355cb0fd0752f9537281d45d8e4c63069ecb0
Deon Gracias <58945356+deon-gracias@users.noreply.github.com>
2022-08-07 07:47:27 +0000

    Changed to https://backend-782.herokuapp.com

--- src/utils/watchlist.js
+++ src/utils/watchlist.js
@@ -13,7 +13,7 @@ export function addToWatchlist(coinId, handleAlert) {
   console.log("Coin Id", coinId);
 
   axios
-    .post("https://currency-backend.herokuapp.com/watchlist/add", {
+    .post("https://backend-782.herokuapp.com/watchlist/add", {
       email: user.email,
       coinId: coinId,
     })
